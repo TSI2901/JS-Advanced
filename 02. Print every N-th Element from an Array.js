1c1d417b0c84dcdd6f7080f95531f849e8935d4c
Anton Ivanov <90517326+TSI2901@users.noreply.github.com>
2022-09-16 18:34:14 +0300

    Delete 02. Print every N-th Element from an Array.js

--- 02. Print every N-th Element from an Array.js
+++
@@ -1,9 +0,0 @@
-function Print(arr,num) {
-    let result =[];
-    for (let i = 0; i < arr.length; i+= num) {
-        result.push(arr[i])
-    }
-    return result;
-}
-Print(['5','20', '31', '4', '20'], 2)
-Print(['dsa', 'asd', 'test', 'tset'], 2)
