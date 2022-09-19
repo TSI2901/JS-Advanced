c26b813b74d9360a9af1687866e6c0678b843dd0
Anton Ivanov <90517326+TSI2901@users.noreply.github.com>
2022-09-19 18:45:20 +0300

    Add files via upload

---
+++ Objects and Composition - Lab/01. City Record.js
@@ -0,0 +1,9 @@
+function CityRecord(name,population,treasury) {
+    let record = {
+        name: name,
+        population: population,
+        treasury: treasury
+    }
+    return record
+}
+CityRecord('Tortuga', 7000, 15000)
