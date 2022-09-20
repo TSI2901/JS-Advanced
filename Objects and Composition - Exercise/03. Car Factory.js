a23e7c3346adf27a3af27d5b627bee40291b6c80
Anton Ivanov <90517326+TSI2901@users.noreply.github.com>
2022-09-20 19:04:48 +0300

    Add files via upload

---
+++ Objects and Composition - Exercise/03. Car Factory.js
@@ -0,0 +1,27 @@
+function solve(object) {
+
+    const wheels = Array(4);
+    object.wheelsize % 2 == 0
+        ? wheels.fill(object.wheelsize - 1, 0, 4)
+        : wheels.fill(object.wheelsize, 0, 4);
+
+
+    function getEngine(power) {
+        if (power > 120) {
+            return { power: 200, volume: 3500 };
+        } else if (power > 90) {
+            return { power: 120, volume: 2400 };
+        } else {
+            return { power: 90, volume: 1800 }
+        }
+    }
+
+    let finalProduct = {
+        model: object.model,
+        engine: getEngine(object.power),
+        carriage: { type: object.carriage, color: object.color },
+        wheels
+    }
+
+    return finalProduct;
+}
