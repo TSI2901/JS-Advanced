ffc43f1075924c5cd2f2e78ee28285fbf56b8e5c
Anton Ivanov <90517326+TSI2901@users.noreply.github.com>
2022-09-20 18:01:00 +0300

    Add files via upload

---
+++ Objects and Composition - Lab/04. Object Factory.js
@@ -0,0 +1,15 @@
+function factory(library, orders) {
+    let result = [];
+
+    for (const order of orders) {
+        let product = order.template;
+
+        for (const part of order.parts) {
+            product[part] = library[part];
+        }
+
+        result.push(product);
+    }
+
+    return result;
+}
