03463725624d5d40e52f53ae980a3c001635e517
Anton Ivanov <90517326+TSI2901@users.noreply.github.com>
2022-09-20 18:56:33 +0300

    Add files via upload

---
+++ Objects and Composition - Exercise/02. Construction Crew.js
@@ -0,0 +1,11 @@
+function solve(worker) {
+    if (worker.dizziness !== true){
+        return worker;
+    }
+    else{
+        worker.levelOfHydrated += (worker.weight * worker.experience * 0.1);
+        worker.dizziness = false;
+        return worker;
+    }
+}
+console.log(solve({ weight: 80, experience: 1, levelOfHydrated: 0, dizziness: true }))
